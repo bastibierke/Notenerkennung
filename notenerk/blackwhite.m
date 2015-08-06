35e2b4c03f707d3fc83314efbf55f2f2ac4b05b9
Felix <uni@localhost.localdomain>
2015-08-06 12:58:11 +0200

    blackwhite.m Bildfarbe geht nur bis 255

--- notenerk/blackwhite.m
+++ notenerk/blackwhite.m
@@ -3,11 +3,11 @@ function M=blackwhite(file,b=.95)
 
 f=imread(file);
 if length(size(f))==3
-    f=sum(f,3)/(3*256);
+    f=sum(f,3)/(3*255);
 end
 
 if sum(sum(f>1.1)>0)
-    f=f/256;
+    f=f/255;
 end
 
 
